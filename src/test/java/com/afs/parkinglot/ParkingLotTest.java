b70a6dc842cac514c3a1327a44185debc33b2887
Lanest <lanest.lin@oocl.com>
2025-09-08 16:37:00 +0800

    test: should_return_correct_car_when_fetch_two_cars

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -25,4 +25,21 @@ public class ParkingLotTest {
         assertEquals(car, carResult);
     }
 
+    //case3 Given a parking lot,two parked cars,two parking ticket When the fetch, Then return correct car
+    @Test
+    public void should_return_correct_car_when_fetch_two_cars() {
+        ParkingLot parkingLot = new ParkingLot(10);
+        Car car1 = new Car("1");
+        Car car2 = new Car("2");
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+        Car carResult1 = parkingLot.fetch(ticket1);
+        Car carResult2 = parkingLot.fetch(ticket2);
+        assertEquals(car1, carResult1);
+        assertEquals(car2, carResult2);
+    }
+
+    //case4 Given a parking lot,a car When the fetch with wrong ticket, Then return null
+    //case5 Given a parking lot,a car When the fetch with used ticket, Then return null
+    //case6 Given a parking lot,a car When the parking lot is no position, Then return null
 }
