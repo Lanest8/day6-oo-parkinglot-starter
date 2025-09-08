11612e895a81066647dd55b47191e3a17c6a8af8
Lanest <lanest.lin@oocl.com>
2025-09-08 16:43:41 +0800

    refactor: clean ParkingLotTest

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -9,7 +9,7 @@ public class ParkingLotTest {
     //case1 Given a parking lot,a car When the car, Then return a parking ticket
     @Test
     public void should_return_ticket_when_parking_a_car() {
-        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingLot parkingLot = new ParkingLot();
         Car car = new Car("park number 1");
         Ticket ticket = new Ticket(car, 1, parkingLot);
         Ticket ticketResult = parkingLot.park(car);
@@ -19,7 +19,7 @@ public class ParkingLotTest {
     //case2 Given a parking lot,a car,a parking ticket When the fetch, Then return car
     @Test
     public void should_return_car_when_fetch_a_car() {
-        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingLot parkingLot = new ParkingLot();
         Car car = new Car("1");
         Ticket ticketResult = parkingLot.park(car);
         Car carResult = parkingLot.fetch(ticketResult);
@@ -29,7 +29,7 @@ public class ParkingLotTest {
     //case3 Given a parking lot,two parked cars,two parking ticket When the fetch, Then return correct car
     @Test
     public void should_return_correct_car_when_fetch_two_cars() {
-        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingLot parkingLot = new ParkingLot();
         Car car1 = new Car("1");
         Car car2 = new Car("2");
         Ticket ticket1 = parkingLot.park(car1);
@@ -43,7 +43,7 @@ public class ParkingLotTest {
     //case4 Given a parking lot,a car When the fetch with wrong ticket, Then return null
     @Test
     public void should_return_null_when_fetch_a_cars_with_wrong_ticket() {
-        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingLot parkingLot = new ParkingLot();
         Car car = new Car("1");
         Ticket ticket = parkingLot.park(car);
         Ticket wrongTicket = new Ticket(new Car("2"), 2, parkingLot);
@@ -54,7 +54,7 @@ public class ParkingLotTest {
     //case5 Given a parking lot,a car When the fetch with used ticket, Then return null
     @Test
     public void should_return_null_when_fetch_a_cars_with_used_ticket() {
-        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingLot parkingLot = new ParkingLot();
         Car car = new Car("1");
         Ticket ticket = parkingLot.park(car);
         Car carResult1 = parkingLot.fetch(ticket);
@@ -63,5 +63,4 @@ public class ParkingLotTest {
         assertNull(carResult2);
     }
 
-    //case6 Given a parking lot,a car When the parking lot is no position, Then return null
 }
