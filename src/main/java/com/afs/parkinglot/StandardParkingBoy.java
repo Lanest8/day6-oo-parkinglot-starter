f1526a1a18e5ac6382ff91da151879d77fc73608
Lanest <lanest.lin@oocl.com>
2025-09-09 09:34:46 +0800

    refactor: extract generateTicket

--- src/main/java/com/afs/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/afs/parkinglot/StandardParkingBoy.java
@@ -14,24 +14,26 @@ public class StandardParkingBoy {
         return parkingLots.stream()
                 .filter(lot -> !lot.isFull())
                 .findFirst()
-                .flatMap(parkingLot -> {
-                    Set<Integer> occupiedPositions = parkingLot.getTicketCars().keySet().stream()
-                            .map(Ticket::getPosition)
-                            .collect(Collectors.toCollection(() -> new HashSet<>(parkingLot.getCapacity())));
-
-                    return IntStream.rangeClosed(1, parkingLot.getCapacity())
-                            .filter(position -> !occupiedPositions.contains(position))
-                            .boxed()
-                            .findFirst()
-                            .map(position -> {
-                                Ticket ticket = new Ticket(car, position, parkingLot);
-                                parkingLot.getTicketCars().put(ticket, car);
-                                return ticket;
-                            });
-                })
+                .flatMap(parkingLot -> generateTicket(car, parkingLot))
                 .orElseGet(() -> {
                     System.out.println("No available position.");
                     return null;
+                });
+    }
+
+    private static Optional<Ticket> generateTicket(Car car, ParkingLot parkingLot) {
+        Set<Integer> occupiedPositions = parkingLot.getTicketCars().keySet().stream()
+                .map(Ticket::getPosition)
+                .collect(Collectors.toCollection(() -> new HashSet<>(parkingLot.getCapacity())));
+
+        return IntStream.rangeClosed(1, parkingLot.getCapacity())
+                .filter(position -> !occupiedPositions.contains(position))
+                .boxed()
+                .findFirst()
+                .map(position -> {
+                    Ticket ticket = new Ticket(car, position, parkingLot);
+                    parkingLot.getTicketCars().put(ticket, car);
+                    return ticket;
                 });
     }
 
